20eba87bc55cba3ea0ee76dad1efa6486e3917c4
Marcela Langarica Mejia <mlangaricamejia@gmail.com>
2023-08-30 17:40:44 -0400

    On the process of creating Document Views, have some modifications to do

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,23 @@
 class DocumentsController < ApplicationController
+
   def new
+    @document = Document.new
+    authorize @document
   end
 
   def create
+    @document = Document.new(document_params)
+    @document.user = current_user
+    @document.save
+    authorize @document
   end
 
   def delete
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:name, :file)
   end
 end
