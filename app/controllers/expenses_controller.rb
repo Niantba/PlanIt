21c973c18f205bd838c5a8224b57e8ef6ca0c821
sergio santana <sergiosantana.m@gmail.com>
2023-08-30 19:05:17 -0400

    expenses files correction

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,15 +1,16 @@
 class ExpensesController < ApplicationController
   def index
-    @expenses = Expense.all
+    @user_trips = current_user.user_trips
+    @expenses = Expense.where(user_trip_id: @user_trips)
     @expenses = policy_scope(Expense)
   end
 
   def create
     @user_trips = UserTrip.find(params[:user_trip_id])
-    @expense = @user_trips.expenses.build(expense_params)
+    @expense = @user_trips.expenses.create(expense_params)
 
     if @expense.save
-      redirect_to expenses_path, notice: "Expense created successfully."
+      redirect_to expenses_path, status: :unprocessable_entity
     else
       render :new
     end
