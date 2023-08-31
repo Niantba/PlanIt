8c1c90b4743e82644573b9c8a8a94ba1b85c599c
Audrey Baniant <audrey.baniant@gmail.com>
2023-08-31 15:57:46 -0400

    Activities don't work yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :user_trips
+  has_many :user_trips, dependent: :destroy
   has_many :trips, through: :user_trips
   has_many :documents
   has_many :comments
