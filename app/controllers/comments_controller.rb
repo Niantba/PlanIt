8b4ad4f6e7c8d3773b424483e8b34e290535e84c
Marcela Langarica Mejia <mlangaricamejia@gmail.com>
2023-09-05 14:31:23 -0400

    adding comments to each activity

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,22 @@
 class CommentsController < ApplicationController
   def create
-    # add instance of activities
+    @trip = Trip.find(params[:trip_id])
+    @activity = Activity.find(params[:activity_id])
+    @comment = Comment.new(comment_params)
+    @comment.activity = @activity
+    @comments = Comment.where(activity: @activity)
+    @comment.user = current_user
+    authorize @comment
+    if @comment.save
+      redirect_to trip_path(@trip)
+    else
+      render "trips/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 end
